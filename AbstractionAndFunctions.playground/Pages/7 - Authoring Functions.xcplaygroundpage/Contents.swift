c90146f62f348486717aef1c229a044898223255
WilliamChan <wchan@lcs.on.ca>
2020-02-12 15:19:23 -0500

    finished creating functions

--- AbstractionAndFunctions.playground/Pages/7 - Authoring Functions.xcplaygroundpage/Contents.swift
+++ AbstractionAndFunctions.playground/Pages/7 - Authoring Functions.xcplaygroundpage/Contents.swift
@@ -35,6 +35,66 @@ func surfaceAreaOfSphere(radius: Double) -> Double {
 }
 
 
+/// Finds the surface area of rectangular prism
+/// - Parameter height: height of rectangular prism
+/// - Parameter length: length of rectangular prism
+/// - Parameter width: width of rectangular prism
+/// - Returns: surface area of rectangular prism
+func surfaceAreaOfRectangularPrism(height:Double, length: Double, width: Double) -> Double{
+    return 2 * (height * length + height * width + length * width)
+}
+
+/// Finds the area of triangle
+/// - Parameter base: base of triangle
+/// - Parameter height: height of triangle
+/// - Returns:area of triangle
+func areaOfTriangle(base: Double, height: Double) -> Double {
+    return 0.5*base*height
+}
+
+
+/// Finds the area of rectangle
+/// - Parameter length: length of rectangle
+/// - Parameter width: width of rectangle
+/// - Returns:area of ractangle
+func areaOfRectangle (length: Double, width: Double) -> Double {
+    return length * width
+}
+
+/// Finds the volume of cylinder
+/// - Parameter radius: radius of base circle
+/// - Parameter height: height of cylinder
+/// - Returns:volume of cylinder
+func volumeOfCylider(radius:Double, height: Double) -> Double{
+    return Double.pi * height * pow(radius, 2.0)
+}
+
+
+/// Finds the volume of square based pyramid
+/// - Parameter base: length of base rectangle
+/// - Parameter height: height of pyramid
+/// - Returns: volume of square-based-pyramid
+func volumeOfSquareBasedPyramid (base: Double, height: Double) -> Double {
+    return pow(base, 2.0) * height / 3
+}
+
+
+/// Finds the circumfrence of circle
+/// - Parameter radius: radius of circle
+/// - Returns: circumfrence of circle
+func circumfrenceOfCircle (radius: Double) -> Double{
+    return 2 * Double.pi * radius
+}
+
+/// Finds the perimeter of rectangle
+/// - Parameter length: length of rectangle
+/// - Parameter Width: width of rectangle
+/// - Returns: perimeter of rectangle
+func perimeterOfRectangle(length: Double, Width: Double) -> Double {
+    return 2*(length + Width)
+}
+
+
 /*:
  [Previous: Writing Documentation](@previous) | Page 7
  */
